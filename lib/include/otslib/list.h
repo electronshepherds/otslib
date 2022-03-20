bffd1b983f7057ecd129979a35376196546f95ab
Abe Kohandel <abe@electronshepherds.com>
2022-03-20 12:30:59 -0700

    Add missing documentation
    Add documentation for the missing components of the library.
    
    Signed-off-by: Abe Kohandel <abe@electronshepherds.com>

--- lib/include/otslib/list.h
+++ lib/include/otslib/list.h
@@ -13,11 +13,55 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/**
+ * @brief Select the first object
+ *
+ * @param adapter an otslib adapter
+ * @retval -EINVAL if @p adapter is invalid
+ * @retval 0 when successful
+ * @retval other negative values for other errors
+ */
 int otslib_first(void *adapter);
+
+/**
+ * @brief Select the last object
+ *
+ * @param adapter an otslib adapter
+ * @retval -EINVAL if @p adapter is invalid
+ * @retval 0 when successful
+ * @retval other negative values for other errors
+ */
 int otslib_last(void *adapter);
+
+/**
+ * @brief Select the previous object
+ *
+ * @param adapter an otslib adapter
+ * @retval -EINVAL if @p adapter is invalid
+ * @retval 0 when successful
+ * @retval other negative values for other errors
+ */
 int otslib_previous(void *adapter);
+
+/**
+ * @brief Select the next object
+ *
+ * @param adapter an otslib adapter
+ * @retval -EINVAL if @p adapter is invalid
+ * @retval 0 when successful
+ * @retval other negative values for other errors
+ */
 int otslib_next(void *adapter);
+
+/**
+ * @brief Select the object with specified ID
+ *
+ * @param adapter an otslib adapter
+ * @param id object ID to select
+ * @retval -EINVAL if @p adapter or @p id is invalid
+ * @retval 0 when successful
+ * @retval other negative values for other errors
+ */
 int otslib_goto(void *adapter, uint64_t id);
 
 #ifdef __cplusplus
